80760d0c54bf275892a94953723095ac1c423c6a
Sam Tygier <sam.tygier@stfc.ac.uk>
2024-08-09 13:31:17 +0100

    Install miniforge

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@
 set -o nounset
 set -o errexit
 
+# Install conda
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
+bash Miniforge3-Linux-x86_64.sh -b
+rm Miniforge3-Linux-x86_64.sh
+
 # Create conda environments
 mamba env create -f https://raw.githubusercontent.com/samtygier-stfc/tomophantomtool/main/environment.yaml
 mamba env create -f https://raw.githubusercontent.com/mantidproject/mantidimaging/stable/environment.yml
