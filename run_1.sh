c37866ce7767d9bf61ecfadf6fa2f08c81a56adb
Sam Tygier <sam.tygier@stfc.ac.uk>
2024-08-09 13:36:17 +0100

    Activate conda

--- run_1.sh
+++ run_1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -o nounset
 set -o errexit
+source ~/miniforge3/etc/profile.d/conda.sh
 
 # Run with 1GB dataset
 mamba run -n mantidimaging python do_recon.py data_1GB/Tomo/Spheres_0000.tiff out1
--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ set -o errexit
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
 bash Miniforge3-Linux-x86_64.sh -b
 rm Miniforge3-Linux-x86_64.sh
+source ~/miniforge3/etc/profile.d/conda.sh
 
 # Create conda environments
 mamba env create -f https://raw.githubusercontent.com/samtygier-stfc/tomophantomtool/main/environment.yaml
