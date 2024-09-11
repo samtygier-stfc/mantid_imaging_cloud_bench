01250f4569d631b768061120e52055bbec7d00cd
Sam Tygier <sam.tygier@stfc.ac.uk>
2024-09-11 09:11:30 +0100

    Add an approx 5GB dataset

--- make_data.sh
+++ make_data.sh
@@ -5,6 +5,7 @@ source ~/miniforge3/etc/profile.d/conda.sh
 
 # Make test data
 mamba run -n tomophantom tomophantomtool/tomophantomtool.py spheres_1GB.yaml data_1GB
+mamba run -n tomophantom tomophantomtool/tomophantomtool.py spheres_5GB.yaml data_5GB
 mamba run -n tomophantom tomophantomtool/tomophantomtool.py spheres_8GB.yaml data_8GB
 
 
---
+++ run_5.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+source ~/miniforge3/etc/profile.d/conda.sh
+
+# Run with 8GB dataset
+mamba run -n mantidimaging python do_recon.py data_5GB/Tomo/Spheres_0000.tiff out5
